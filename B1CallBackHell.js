53931c17a0f2766b836cce4a92b465cc6bf93e31
Ofir Ben Yacov <ofirby93@gmail.com.(none)>
2022-08-04 08:14:48 +0300

    On branch Master Your branch is up to date with 'origin/Master'.

--- B1CallBackHell.js
+++ B1CallBackHell.js
@@ -4,13 +4,9 @@ export function getDataCallBack() {
   console.log('Request is sending now')
   const div = $('#callBackDiv')
   div.html('')
-
-  const storesObj = {}
-
+  const depart = []
   $.get('http://localhost:3000/stores')
     .done(function (stores) {
-      console.log('stores')
-      console.log(stores)
       for (const store of stores) {
         $.ajaxSetup({
           contentType: 'application/json; charset=UTF-8',
@@ -20,10 +16,9 @@ export function getDataCallBack() {
           JSON.stringify(store.departments)
         )
           .done(function (departments) {
-            console.log('departments')
-            console.log(departments)
+            store.departments = departments
             for (const num in departments) {
-              store.departments[num] = departments[num]
+              depart.push(departments[num].name)
               $.ajaxSetup({
                 contentType: 'application/json; charset=UTF-8',
               })
@@ -32,13 +27,15 @@ export function getDataCallBack() {
                 'http://localhost:3000/bulk-products',
                 JSON.stringify(departments[num].products)
               )
+            
                 .done(function (products) {
-                  console.log('products')
-                  console.log(products)
                   for (const key in products) {
                     departments[num].products[key] = products[key].name
-                    // addStoreToHtml(store, departments, div)
+                    const dep = store.departments
+                    getDataToHTML(store, div) 
                   }
+                  
+              
                 })
                 .fail(function (err) {
                   console.log(err)
@@ -50,21 +47,45 @@ export function getDataCallBack() {
             console.log(err)
             console.log(err.status)
           })
-          console.log(store)
       }
+      
+      console.log(stores);
+      // vanillaDataToHtml(stores, div)
+      // div.append(`<li>${stores.name}<li/>`)
+      // getDataToHTML(stores, depart, div)
     })
     .fail(function (err) {
       console.log(err)
       console.log(err.status)
     })
-    console.log(storesObj);
-}
+  }
 
-function addStoreToHtml(store, element) {
-  const 
-  element.append( `<h2>${store.name}</h2>`
-      `<ol>${store.departments}
-        <li>trew</li>
-    </ol>`  
-  )
-}
+
+  //function getDataToHTML(data, depart, location) {
+  function getDataToHTML(store, location) {
+    // const div = document.createElement('div')
+    
+    if($('#Rami Levi')){
+      div.append(`<h2 id="${store.name}">${store.name}</h2>`)
+      const departments = store.departments
+      for(let i = 0; i < departments.length; i++){
+
+      }
+    }
+    // console.log(`data`);
+    // console.log(data);
+    // for (let i = 0; i < data.length; i++) {
+    //   const h2 = document.createElement('h2')
+    //   h2.innerHTML = data[i].name
+    //   console.log(depart)
+    //   const departments = data[i].departments
+    //   console.log(departments);
+    //   for(const department of departments){
+    //     const ol = document.createElement('ol')
+    //     ol.innerHTML = department.name
+    //     h2.append(ol)
+    //   }
+    //   div.append(h2)
+    // }
+    location.append(div)
+  }
